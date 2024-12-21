9fa104dc937b1f2ee2953a81af37b2e3cffea1af
Mohammad Shariq Pir <pirshariq419@gmail.com>
2024-12-21 07:07:49 +0530

    first commit

--- script.js
+++ script.js
@@ -1,4 +1,37 @@
 function toggleMenu() {
     const menu = document.querySelector('.navbar-menu');
-    menu.classList.toggle('show');
+    const body = document.body;
+
+    menu.classList.toggle('show'); 
+
+    if (menu.classList.contains('show')) {
+        body.classList.add('no-scroll'); 
+    } else {
+        body.classList.remove('no-scroll'); 
+    }
 }
+
+
+
+document.querySelector('.backdrop').addEventListener('click', function() {
+    const menu = document.querySelector('.navbar-menu');
+    const backdrop = document.querySelector('.backdrop');
+    menu.classList.remove('show');
+    backdrop.classList.remove('show');
+    document.body.style.overflow = 'auto';  
+});
+
+function toggleMenu() {
+    const menu = document.querySelector('.navbar-menu');
+    const backdrop = document.querySelector('.backdrop');
+    menu.classList.toggle('show');
+    
+    if (menu.classList.contains('show')) {
+        document.body.style.overflow = 'hidden';
+        backdrop.classList.add('show');
+    } else {
+        document.body.style.overflow = 'auto';
+        backdrop.classList.remove('show');
+    }
+}
+
